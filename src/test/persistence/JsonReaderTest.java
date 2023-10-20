c402a3cd26953748d0e18e39c3f9ba798eee9772
michaelyang <mich207@student.ubc.ca>
2023-10-19 22:22:30 -0700

    Fully implemented all of the tests for the JsonReader and Writer.

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,11 +1,63 @@
 package persistence;
 
+import model.*;
 import org.junit.jupiter.api.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Took inspiration from JsonSerializationDemo document found at
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonReaderTest {
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/727WYSI.json");
+        try {
+            Hand hand = reader.readHand();
+            DiscardedTiles discards = reader.readDiscards();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // expected
+        }
+    }
+
+    @Test
+    void testReaderEmptyGame() {
+        JsonReader reader = new JsonReader("./data/testReaderEmptyGame.json");
+        try {
+            Hand hand = reader.readHand();
+            DiscardedTiles discards = reader.readDiscards();
+            assertEquals(0, hand.getHandLength());
+            assertEquals(0, discards.getDiscardedTiles().size());
+        } catch (IOException e) {
+            fail("Oh nyooo!!!! couldn't read file!!");
+        }
+    }
+
+    @Test
+    void testReaderGeneralGame() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralGame.json");
+        try {
+            Hand hand = reader.readHand();
+            DiscardedTiles discards = reader.readDiscards();
+
+            List<Tile> handList = hand.getHand();
+            assertEquals(14, handList.size());
+            for (int index = 0; index < 14; index++) {
+                assertEquals(index,handList.get(index).getID());
+            }
+
+            List<Tile> discardsList = discards.getDiscardedTiles();
+            assertEquals(14, discardsList.size());
+            for (int index = 0; index < 14; index++) {
+                assertEquals(2*index,handList.get(index).getID());
+            }
+
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
 }
--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -1,11 +1,102 @@
 package persistence;
 
+import model.DiscardedTiles;
+import model.Hand;
+import model.Tile;
 import org.junit.jupiter.api.*;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+// Took inspiration from JsonSerializationDemo document found at
+// https://github.students.cs.ubc.ca/CPSC210/JsonSerializationDemo
 public class JsonWriterTest {
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            Hand testHand = new Hand();
+            DiscardedTiles testDiscards = new DiscardedTiles();
+            JsonWriter writer = new JsonWriter("./data/myWrit/\135[145[Sff.json");
+            writer.open();
+            fail("IOException was expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyWorkroom() {
+        try {
+            List<Tile> handList = new LinkedList<>();
+            Hand testHand = new Hand(handList);
+            DiscardedTiles testDiscards = new DiscardedTiles();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyGame.json");
+            writer.open();
+            writer.write(testHand, testDiscards);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyGame.json");
+            Hand checkHand = reader.readHand();
+            DiscardedTiles checkDiscards = reader.readDiscards();
+            assertEquals(0, checkHand.getHandLength());
+            assertEquals(0, checkDiscards.getDiscardedTiles().size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralWorkroom() {
+
+        List<Tile> testHandList = new LinkedList<>();
+        for (int i = 0; i < 14; i++) {
+            testHandList.add(new Tile(i + 3));
+        }
+        Hand testHand = new Hand(testHandList);
+
+        DiscardedTiles testDiscards = new DiscardedTiles();
+        for (int i = 0; i < 9; i++) {
+            testDiscards.addTile(new Tile(i + 25));
+        }
+        List<Tile> testDiscardsList = testDiscards.getDiscardedTiles();
+
+        JsonWriter writer = new JsonWriter("./data/testWriterGeneralGame.json");
+        try {
+            writer.open();
+            writer.write(testHand, testDiscards);
+            writer.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException("can't write that!!!!");
+        }
+
+        JsonReader reader = new JsonReader("./data/testWriterGeneralGame.json");
+        try {
+            Hand checkHand = reader.readHand();
+            DiscardedTiles checkDiscards = reader.readDiscards();
+
+            List<Tile> checkHandList = checkHand.getHand();
+            assertEquals(testHandList.size(), checkHandList.size());
+            int index = 0;
+            for (Tile tile : testHandList) {
+                assertEquals(tile.getID(), checkHandList.get(index).getID());
+                index++;
+            }
+
+            List<Tile> checkDiscardsList = checkDiscards.getDiscardedTiles();
+            assertEquals(testDiscardsList.size(), checkDiscardsList.size());
+            index = 0;
+            for (Tile tile : testDiscardsList) {
+                assertEquals(tile.getID(), checkDiscardsList.get(index).getID());
+                index++;
+            }
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
 }
